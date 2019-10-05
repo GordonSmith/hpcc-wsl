641ffbb828cce50142fb3cc480402f5749bb5e1a
Gordon Smith <gordonjsmith@gmail.com>
2019-10-05 16:39:34 +0100

    feat: Initial commit
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ resource.h
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <shellapi.h>
+
+#define ICO1 101
+#define ID_TRAY_APP_ICON 1001
+#define ID_TRAY_START 1002
+#define ID_TRAY_STOP 1003
+#define ID_TRAY_STATUS 1004
+#define ID_TRAY_SEP 1005
+#define ID_TRAY_EXIT 1006
+#define WM_SYSICON (WM_USER + 1)
